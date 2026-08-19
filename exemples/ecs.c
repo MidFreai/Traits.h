9c7e01f0fe382a8a6d495eb7cfecfaa432c4e39f
MidFreai <MidFreai@gmail.com>
2026-08-19 03:26:24 -0300

    exemple ecs

---
+++ exemples/ecs.c
@@ -0,0 +1,23 @@
+#define TRAIT_IMPL
+#include <trait.h>
+#include <stdio.h>
+
+typedef struct{
+  float x;
+  float y;
+}Position;
+static const char Position_id;
+
+void printPosition(void* self){
+  Position* pos = trait_find(self, &Position_id);
+  if(pos == NULL) return;
+  printf("Position: x %f, y %f\n", pos->x, pos->y);
+}
+
+int main(void){
+  Trait_entity te;
+
+  newTrait(&te, &Position_id, &(Position){ 3, 4 });
+
+  printPosition(&te);
+}
